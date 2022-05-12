5133d43b28f507f16cee99222c5c30a3daa38fee
Avinash3241 <102507619+Avinash3241@users.noreply.github.com>
2022-05-12 22:09:27 +0530

    Add files via upload

---
+++ codes/Assign1.py
@@ -0,0 +1,3 @@
+import math
+x = math.sin(7*math.pi/45)**2 + math.sin(31*math.pi/90)**2 +math.tan(19*math.pi/90)**2 - ((math.cos(13*math.pi/45)**2)/(math.sin(13*math.pi/45)**2)) + (1/(math.cos(math.pi/6)**2))
+print(x)
